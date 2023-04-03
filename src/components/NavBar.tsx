9a39769623ace2bad3a5625740a301d6e17dbe47
Sergey Tulonov <me@k3env.site>
2023-04-03 10:49:02 +0800

    feat: add authorized/unauthorized state to navbar feat: add auth check for each route change feat: replace nav dropdown with simple nav links in navbar fix: add 'collapseOnSelect' prop to navbar for closing on route change

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,46 +1,62 @@
-import { Container, Nav, Navbar, NavDropdown } from "react-bootstrap";
+import { Container, Nav, Navbar } from "react-bootstrap";
 import { Link } from "atomic-router-react";
-import { Home, Profile, Tags, Gallery, Projects } from "~routes";
+import { Home, Profile, Tags, Gallery, Projects, Login } from "~routes";
+import { useEffect, useState } from "react";
+import { retry } from "../helpers/interceptor";
+import { historyWatch } from "../routes/router";
 
 export function NavBar(props: {}) {
+  const [user, setUser] = useState<string | undefined>(undefined);
+  const [path, setPath] = useState<string>("");
+
+  historyWatch((u) => setPath(u.location.pathname));
+
+  useEffect(() => {
+    retry(
+      "GET",
+      `${import.meta.env.VITE_AUTH_URL}/user/me`,
+      undefined,
+      (rUser) => setUser(rUser.data.username),
+      () => Login.route.open()
+    );
+  }, [path]);
   return (
-    <Navbar bg="light" expand="lg" className="shadow" sticky="top">
+    <Navbar
+      bg="light"
+      expand="lg"
+      className="shadow"
+      sticky="top"
+      collapseOnSelect
+    >
       <Container>
-        <Navbar.Brand href="#home">
+        <Navbar.Brand>
           <span className="text-primary fw-bold">K3</span>Env's admin UI
         </Navbar.Brand>
         <Navbar.Toggle aria-controls="navbar-nav" />
-        <Navbar.Collapse id="navbar-nav">
-          <Nav className="me-auto">
-            <Nav.Link as={Link} to={Home.route}>
-              Home
-            </Nav.Link>
-            <Nav.Link as={Link} to={Gallery.route}>
-              Gallery
-            </Nav.Link>
-            {/* <Nav.Link as={Link} to={Profile.route}>
-              Profile
-            </Nav.Link>
-            <Nav.Link as={Link} to={Tags.route}>
-              Tags
-            </Nav.Link>
-            <Nav.Link as={Link} to={Projects.route}>
-              Projects
-            </Nav.Link> */}
-            <NavDropdown title="Models" id="nav-dropdown">
-              <NavDropdown.Item as={Link} to={Profile.route}>
+        {user && (
+          <Navbar.Collapse id="navbar-nav">
+            <Nav className="me-auto">
+              <Nav.Link as={Link} to={Home.route}>
+                Home
+              </Nav.Link>
+              <Nav.Link as={Link} to={Gallery.route}>
+                Gallery
+              </Nav.Link>
+              <Nav.Link as={Link} to={Profile.route}>
                 Profile
-              </NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item as={Link} to={Tags.route}>
+              </Nav.Link>
+              <Nav.Link as={Link} to={Tags.route}>
                 Tags
-              </NavDropdown.Item>
-              <NavDropdown.Item as={Link} to={Projects.route}>
+              </Nav.Link>
+              <Nav.Link as={Link} to={Projects.route}>
                 Projects
-              </NavDropdown.Item>
-            </NavDropdown>
-          </Nav>
-        </Navbar.Collapse>
+              </Nav.Link>
+            </Nav>
+            <Nav>
+              <Nav.Link>{user}</Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        )}
       </Container>
     </Navbar>
   );
